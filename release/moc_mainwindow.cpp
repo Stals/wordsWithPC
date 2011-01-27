4591bc0b7e852f4e9d6bb54024049f1993d360b1
Stals <superstals@gmail.com>
2011-01-27 12:08:25 +0300

    PereConpiled i guess

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sat 20. Nov 16:30:12 2010
+** Created: Sat 20. Nov 19:06:37 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.3)
 **
 ** WARNING! All changes made in this file will be lost!
--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Sat 20. Nov 18:45:22 2010
+** Created: Sat 20. Nov 19:06:32 2010
 **      by: Qt User Interface Compiler version 4.6.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
