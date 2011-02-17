bc965ca14a8a9f2bb06ae48c0313a247938ae5c2
Stals <superstals@gmail.com>
2011-02-17 22:07:39 +0300

    вектор файлов состявляется в процессе работы

--- findword.cpp
+++ findword.cpp
@@ -2,14 +2,24 @@
 #include <algorithm>
 #include <cstdlib>
 #include <string.h>
+#include <windows.h>
 findWord::findWord()
 {
     loadDicts();
 }
 void findWord::loadDicts(){
 
-     std::string filesArray[] =  { "dict//1","dict//2","dict//3","dict//4","dict//5","dict//6","dict//7","dict//8","dict//9","dict//10","dict//11","dict//12","dict//13","dict//14","dict//15","dict//16","dict//17","dict//18","dict//19","dict//20","dict//21","dict//22","dict//23","dict//24","dict//25","dict//26","dict//27","dict//28","dict//29","dict//30","dict//31","dict//32","dict//33" };
-     std::vector<std::string> files (filesArray, filesArray + sizeof(filesArray) / sizeof(std::string) );
+     //std::string filesArray[] =  { "dict//1","dict//2","dict//3","dict//4","dict//5","dict//6","dict//7","dict//8","dict//9","dict//10","dict//11","dict//12","dict//13","dict//14","dict//15","dict//16","dict//17","dict//18","dict//19","dict//20","dict//21","dict//22","dict//23","dict//24","dict//25","dict//26","dict//27","dict//28","dict//29","dict//30","dict//31","dict//32","dict//33" };
+   //std::vector<std::string> files (filesArray, filesArray + sizeof(filesArray) / sizeof(std::string) );
+    std::vector<std::string> files;
+    char buf[3];
+    for(int i=1 ; i<34 ; ++i){
+          std::string str="dict//";
+          itoa(i,buf,10);
+          str.append(buf);
+          files.push_back(str);
+    }
+
      for( std::vector<std::string>::iterator it=files.begin();it!=files.end();++it){
          std::ifstream f((*it).c_str());
          int num;//number of words
@@ -27,6 +37,7 @@ void findWord::loadDicts(){
          dictionary.insert ( std::pair<char,std::list<std::string> >(letter[0],singleDict) );
          singleDict.clear();
       }
+
 
 }
 void findWord::usedWord(std::string Word,char lastLetter){
