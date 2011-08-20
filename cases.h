3ce171517edb6cfcd58afd8e73e1921bc2968d7d
Stals <superstals@gmail.com>
2011-08-20 15:19:17 +0400

    added spaces

--- cases.h
+++ cases.h
@@ -8,7 +8,7 @@ namespace cases{
 inline std::string toLowerCase(std::string& word){
     for(size_t i = 0; i < word.length(); ++i){
         //NOTE : При сборке в новом qt 4.7 приводит к нижнему регистру даже русские буквы
-        word[i]=tolower( word[i] );
+        word[i] = tolower( word[i] );
     }
     return word;
 }
