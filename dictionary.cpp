f7e4a49dc3a1e5a6f3e6ce0846c383ac7e6cb57c
Stals <superstals@gmail.com>
2011-08-23 20:02:07 +0400

    new words are now save for later use by PC

--- dictionary.cpp
+++ dictionary.cpp
@@ -7,11 +7,63 @@
 
 #include <iterator>
 Dictionary::Dictionary(){
+    //Загружаем словари
     loadDicts();
+
+    //заполняем map charToNum значениями
+    charToNum['а'] = "1";
+    charToNum['б'] = "2";
+    charToNum['в'] = "3";
+    charToNum['г'] = "4";
+    charToNum['д'] = "5";
+    charToNum['е'] = "6";
+    charToNum['ё'] = "6";//Слова на ё сохраняем в словарь е
+    charToNum['ж'] = "8";
+    charToNum['з'] = "9";
+    charToNum['и'] = "10";
+    charToNum['й'] = "10";//Слова на й сохраняем в словарь и
+    charToNum['к'] = "12";
+    charToNum['л'] = "13";
+    charToNum['м'] = "14";
+    charToNum['н'] = "15";
+    charToNum['о'] = "16";
+    charToNum['п'] = "17";
+    charToNum['р'] = "18";
+    charToNum['с'] = "19";
+    charToNum['т'] = "20";
+    charToNum['у'] = "21";
+    charToNum['ф'] = "22";
+    charToNum['х'] = "23";
+    charToNum['ц'] = "24";
+    charToNum['ч'] = "25";
+    charToNum['ш'] = "26";
+    charToNum['щ'] = "27";
+    charToNum['ъ'] = "28";
+    charToNum['ы'] = "29";
+    charToNum['ь'] = "30";
+    charToNum['э'] = "31";
+    charToNum['ю'] = "32";
+    charToNum['я'] = "33";
+
 }
 Dictionary::~Dictionary(){
-    //TODO добавление слов из newWords в словари компьютера
-    //слово на ё й добавлем в словари е и  и при этом не меням букву
+    //добавление слов из newWords в словари компьютера
+    //слово на ё й добавлем в словари е и (это предусмотрено в charToNum)
+
+    for(unsigned i = 0; i < newWords.size(); ++i){
+        const std::string newWord = newWords[i];
+        const std::string fileName = ("dict//" + charToNum[ newWord[0] ]).c_str();
+
+        std::ofstream f(fileName.c_str(), std::ios::app);
+        f << ("\n"+newWord).c_str();
+        f.close();
+    }
+
+    //DEBUG
+    std::ofstream file("newWords.txt");
+    std::ostream_iterator<std::string> beg(file, "\n");
+    std::copy(newWords.begin(), newWords.end(), beg);
+    //END OF DEBUG
 }
 
 void Dictionary::loadDicts(){ //TODO составить список сопоставлений а -1 б -2 c с помощью map, чтобы потом можно было обратиться по мэпу и понять что для буквы 'в' нужно число 3
